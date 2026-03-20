b59d5011ab1d1485511deec400533f0191d27774
Al Randolph Villegas <villegasalrandolph@gmail.com>
2026-03-20 21:36:18 +0800

    Changed the host address

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 
 
 def main():
-    uvicorn.run("src.server:app", host='127.0.0.1', port=5000, reload=True)
+    uvicorn.run("src.server:app", host='0.0.0.0', port=5000, reload=True)
 
 if __name__ == "__main__":
     main()
