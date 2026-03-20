1c6ee7cc489182798aab6fd228d3fe38ba8cb220
Al Randolph Villegas <villegasalrandolph@gmail.com>
2026-03-21 00:48:27 +0800

    Added download files functionality

--- src/server.py
+++ src/server.py
@@ -1,18 +1,17 @@
-from fastapi import FastAPI, Form, Query
-from typing import Annotated, Optional
+from fastapi import FastAPI, Form, Query, UploadFile, File, Path
+from typing import Annotated, Optional, List
 from pydantic import BaseModel
 from fastapi.requests import Request
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import os
+import shutil
+import mimetypes
 
 
 class Directory(BaseModel):
     directory_name: str
-
-class File(BaseModel):
-    file_name: str
 
 app = FastAPI()
 
@@ -40,6 +39,31 @@ def Home(request: Request, directory_exist: Optional[bool] = Query(default=False
         "directory_exist": directory_exist
     })
 
+@app.get("/view_files/{dir_name}")
+def View_Files(dir_name: str, request: Request):
+
+    files: List[str] = os.listdir(f"file_server_directory/{dir_name}")
+
+    return template.TemplateResponse(request, "view_files.html", {
+
+        "directory_name" : dir_name,
+        "files": files
+
+    })
+
+@app.get("/download/{directory_name}/{file_name}")
+def Download_File(directory_name: str, file_name: str):
+
+    mime_type, _ = mimetypes.guess_type("file_server_directory/{directory_name}/{file_name}")
+
+    return FileResponse(
+
+        path=f"file_server_directory/{directory_name}/{file_name}",
+        filename=file_name,
+        media_type=mime_type or "application/octet-stream"
+    )
+
+
 @app.post("/mkdir")
 def Mkdir(data: Annotated[Directory, Form()]):
 
@@ -64,14 +88,17 @@ def Remove_Directory(dir_name: Optional[str] = Query(default=None)):
     
     else:
         
-        os.rmdir(f"file_server_directory/{dir_name}")
+        shutil.rmtree(f"file_server_directory/{dir_name}")
 
         return RedirectResponse(url="/", status_code=303)
      
 
 @app.post("/upload")
-def Upload_Files(file: Annotated[File, Form()]):
-    
-    print(file.file_name) 
+async def Upload_Files(dir_name: Annotated[str, Form()], file_name: UploadFile = File(...)):
 
-    return {"Message": "Hello"}
+    target_directory = os.path.join("file_server_directory", dir_name, file_name.filename)
+
+    with open(target_directory, "wb") as buffer:
+        shutil.copyfileobj(file_name.file, buffer)
+    
+    return RedirectResponse(url="/", status_code=303)
